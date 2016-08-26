ece06dbce5b0d378905350fd95f7beb8a2ba1bd7
Shiro <shiro.archerl@gmail.com>
2016-08-26 09:33:41 -0500

    Update curling.hs

--- Haskell/Ejercicios/curling.hs
+++ Haskell/Ejercicios/curling.hs
@@ -4,3 +4,9 @@ operacion _ _ [] = []
 operacion (hx:tx) (hy:ty) = [f hx hy] ++ operacion f tx ty
 
 -- Hacer una funcion que reciba un predicado y encuentre el valor que se busca (>) 3 [1,2,5,6]
+comparamela f i lista = [x | x<-lista, x `f` i]
+
+-- Otra forma de hacer el anterior
+coparamesta f (h:t) 
+          | f h = h:comparamesta f t 
+          | otherwise = comparamesta f t
