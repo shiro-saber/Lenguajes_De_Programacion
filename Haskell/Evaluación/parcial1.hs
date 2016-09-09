096706e9af5ae7f4cf8385e5f91ef349834cfc90
shiro-saber <shiro.archerl@gmail.com>
2016-09-09 09:43:36 -0500

    Inicio del palindromo violador

--- Haskell/Evaluación/parcial1.hs
+++ Haskell/Evaluación/parcial1.hs
@@ -1,8 +1,14 @@
 -- Realizar la funcion de fibonacci
-fib 0 = 1
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+kuz 0 = 1
+kuz 1 = 1
+kuz n = kuz (n-1) + kuz (n-2)
 
 -- Un número palíndromo es aquel que se lee igual de izquierda a derecha que de derecha a izquierda.
 -- El palíndromo más grande formado por el producto de dos enteros de dos dígitos es el número 9009 (formado a partir del producto de 91X99).
 -- Cuál es el palíndromo más grande formado a partir del producto de dos números de 3 dígitos?
+
+-- Con esto voy a dar la vuelta al int para comparar
+reverseInt n = aux (n,0)
+             where aux (0,y) = y
+                   aux (x,y) = let (x',y') = x `quotRem` 10
+                               in aux (x',10*y+y')
