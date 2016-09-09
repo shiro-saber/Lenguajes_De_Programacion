7e655dd7c0498e229ef7e7ed2a8cb9d7f33613d6
shiro-saber <shiro.archerl@gmail.com>
2016-09-09 09:18:04 -0500

    Fibonacci

---
+++ Haskell/Evaluación/parcial1.hs
@@ -0,0 +1,8 @@
+-- Realizar la funcion de fibonacci
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+-- Un número palíndromo es aquel que se lee igual de izquierda a derecha que de derecha a izquierda.
+-- El palíndromo más grande formado por el producto de dos enteros de dos dígitos es el número 9009 (formado a partir del producto de 91X99).
+-- Cuál es el palíndromo más grande formado a partir del producto de dos números de 3 dígitos?
