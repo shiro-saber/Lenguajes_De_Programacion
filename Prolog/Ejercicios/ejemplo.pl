cdd28d9cd3ebd2e3ee569c22a860b76f135b8ac1
shiro-saber <shiro.archerl@gmail.com>
2016-10-04 09:38:59 -0500

    ahi va esta

--- Prolog/Ejercicios/ejemplo.pl
+++ Prolog/Ejercicios/ejemplo.pl
@@ -6,6 +6,8 @@ father(terach, abraham).
 father(haren, lot).
 father(haren, mismcah).
 father(haren, yiscah).
+father(abraham, isaac).
+father(tareh, sarah).
 mother(sarah, isaac).
 male(terach).
 male(haran).
@@ -29,4 +31,19 @@ female(yiscah).
 
 % esto es una regla
 son(X,Y) :- father(Y,X),male(X).
-% si corremos son(X, haren). regresa a lot 
+son(X,Y) :- mother(Y,X),male(X).
+% si corremos son(X, haren). regresa a lot
+
+grandFather(X,Y) :- father(X,Z),father(Z,Y).
+grandFather(X,Y) :- father(X,Z),mother(Z,Y).
+
+parent(X,Y) :- father(X,Y).
+parent(X,Y) :- mother(X,Y).
+
+grandParent(X,Y) :- parent(X,Z),parent(Z,Y).
+
+brother(X,Y) :- parent(Z,X),parent(Z,Y), male(Y), X\=Y.
+
+% Recursion en reglas porque yolo
+ancestor(X,Y) :- parent(X,Y). %caso base
+ancestor(X,Y) :- parent(X,Z),ancestor(Z,Y). %paso recursivo
