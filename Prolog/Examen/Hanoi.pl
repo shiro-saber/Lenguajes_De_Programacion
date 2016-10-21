161f1b29c72275232becd7e0114612023da6454e
shiro-saber <shiro.archerl@gmail.com>
2016-10-21 09:47:07 -0500

    Hanoi

--- Prolog/Examen/Hanoi.pl
+++ Prolog/Examen/Hanoi.pl
@@ -1,11 +1,7 @@
 % Torres de Hanoi
-hanoi(N,[H|T],[],[],D1,D2,D3) :- N > 1, M is N-1, hanoi(N, [H|T],[H|_],[],D1,D2,D3),
-                                                  hanoi(M,[_|M],[H|_],[H,_],D1,D2,D3),
-                                                  hanoi(M,[_|M],[],[H|T],D1,D2,D3),
-                                                  hanoi(M,[],[_|T],[],[H|T],D1,D2,D3),
-                                                  hanoi(M,[H|_],[_|T],[H|T],D1,D2,D3),
-                                                  hanoi(M,[H|_],[_|T],[],D1,D2,D3),
-                                                  hanoi(M,[],[H|T]m[],D1,D2,D3),
-                                                  hanoi(N,[H|T],[],[]).
-
-hanoi(N,[H|T],[],[]) :- write(D1), nl, write(D2), nl, write(D3), nl. 
+hanoi(N) :- muevemela(N,left,center,right).
+ 
+muevemela(0,_,_,_) :- !.
+muevemela(N,A,B,C) :- M is N-1, muevemela(M,A,C,B), inform(A,B), muevemela(M,C,B,A).
+ 
+inform(X,Y) :- write([movi,el,disco,desde,X,hasta,Y]), nl.
