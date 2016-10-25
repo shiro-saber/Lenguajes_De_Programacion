10fcd8fcdae28f017e19b185e7aaec9616f016e2
Shiro <shiro.archerl@gmail.com>
2016-10-25 08:49:26 -0500

    Update hanoi.pl

--- Prolog/Examen/hanoi.pl
+++ Prolog/Examen/hanoi.pl
@@ -2,6 +2,6 @@
 hanoi(N) :- muevemela(N,izquierda,centro,derecha).
  
 muevemela(0,_,_,_) :- !.
-muevemela(N,A,B,C) :- M is N-1, muevemela(M,A,C,B), inform(A,B), muevemela(M,C,B,A).
+muevemela(N,A,B,C) :- M is N-1, muevemela(M,A,C,B), inform(A,B,N), muevemela(M,C,B,A).
  
-inform(X,Y) :- write([movi,el,disco,de,X,a,Y]), nl.
+inform(X,Y,N) :- write([movi,el,disco,N,de,X,a,Y]), nl.
