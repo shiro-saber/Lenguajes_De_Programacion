3efc4cc1ff7f77b9ca7c1e11e382e389556f3ce1
shiro-saber <shiro.archerl@gmail.com>
2016-11-01 09:32:42 -0600

    pene

--- Prolog/Ejercicios/maplist.pl
+++ Prolog/Ejercicios/maplist.pl
@@ -1,3 +1,3 @@
 myMapList(_,[],[]).
-myMapList(Func,[H|T],[R|T2]):- Func=..[Func|Params],append(Params,[H,R],NewParams),Pred=..[Func|NewParams],call(Pred),myMapList(Func,T,T2).
+myMapList(Func,[H|T],[R|T2]):- Func=..[Functor|Params],append(Params,[H,R],NewParams),Pred=..[Functor|NewParams],call(Pred),myMapList(Func,T,T2), !.
 succ(In,Out):- Out is In +1.
