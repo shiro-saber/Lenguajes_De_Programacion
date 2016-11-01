3f2f780f099662c1cd21e0259ba2c041a709d613
shiro-saber <shiro.archerl@gmail.com>
2016-11-01 09:33:19 -0600

    replace

--- Prolog/Ejercicios/maplist.pl
+++ Prolog/Ejercicios/maplist.pl
@@ -1,3 +1,5 @@
+replace(X,Y,Z,W) :- (X == Z, W is Y,!);W is Z.
+
 myMapList(_,[],[]).
 myMapList(Func,[H|T],[R|T2]):- Func=..[Functor|Params],append(Params,[H,R],NewParams),Pred=..[Functor|NewParams],call(Pred),myMapList(Func,T,T2), !.
 succ(In,Out):- Out is In +1.
